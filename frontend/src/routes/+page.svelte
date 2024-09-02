d8a66d997ae579b15db07ef5446449fba5877bdb
Khdulkadir <khdulkadir@gmail.com>
2024-09-02 16:11:45 +0200

    Aanpassingen installatie Sveltekit

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>{data.global.title}</h1>
+<p>{data.global.description}</p>
