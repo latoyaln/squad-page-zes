f49aec31350b68a4d4e38c7e6e21c7e3d532a3d2
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-09-12 00:15:35 +0200

    font & custom properties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,16 @@
   <!-- {#each data.squads as squad}
     <h2><a href="/squad/{squad.id}">Squad {squad.name}</a></h2>
   {/each} -->
+
+  <section id="overview"></section>
+  <section id=""></section>
+  <section></section>
+  <section></section>
   <h2><a href="/squad/4">Squad 2C</a></h2>
   <h2><a href="/squad/5">Squad 2D</a></h2>
   <h2><a href="/squad/3">Squad overig</a></h2>
 </main>
 
 <style>
-  a {
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    gap:.5rem;
-    margin-bottom: 1rem;
-    border-radius: .25rem;
-    border:1px solid var(--primary);
-    overflow:hidden;
-    padding:0 0 .25rem;
-    text-decoration:none;
-  }
-  a:hover {
-    background-color: red;
-  }
+  
 </style>
