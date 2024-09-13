0c39185c5b640ae5000823b65f1b0fd8c6bb66af
Khdulkadir <khdulkadir@gmail.com>
2024-09-13 15:11:26 +0200

    Update detailpagina

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -3,11 +3,27 @@
 </script>
 
 <main>
-  <a class="return" href="/">Return</a>
+  <a class="return" href="/squad/{data.persons[0].squad_id}">Return</a>
   <div class="pokeball"></div>
   <article class="card">
     <h1>{data.persons[0].name} {data.persons[0].prefix} {data.persons[0].surname}</h1>
-    <h2>{data.persons[0].id}</h2>
+    <div class="profile-picture-container">
+      <img class="profile-picture" src="{data.persons[0].avatar}" alt="{data.persons[0].name} {data.persons[0].prefix} {data.persons[0].surname}" width="100" height="100">
+    </div>
+    <div class="info-container">
+      <div class="nickname-container">
+        <img class="energy" src="http://res.cloudinary.com/bpettis/image/upload/v1516916046/firetype_otgmwq.png" alt="Fire Energy Type Symbol" width="20" height="20">
+        <p class="nickname">{data.persons[0].nickname}</p>
+      </div>
+      <div class="github-container">
+        <img class="energy" src="http://res.cloudinary.com/bpettis/image/upload/v1516916049/normaltype_ztufzq.png" alt="Normal Energy Type Symbol" width="20" height="20">
+        <p class="github">{data.persons[0].github_handle}</p>
+      </div>
+      <div class="github-container">
+        <img class="energy" src="http://res.cloudinary.com/bpettis/image/upload/v1516916052/watertype_nkics0.png" alt="Water Energy Type Symbol" width="20" height="20">
+        <a class="website" href="{data.persons[0].website}" target="_blank">Website</a>
+      </div>
+    </div>
   </article>
 </main>
 
@@ -29,21 +45,71 @@
 
   .card {
     height: 400px;
-    width: 200px;
+    width: 250px;
     background-image: url(https://img00.deviantart.net/0614/i/2012/106/a/e/classic_pkmn_tcg_texture_pack_by_flamingclaw-d4wcj31.png);
     background-size: cover;
-    display: flex;
     flex-direction: column;
-    justify-content: center;
     gap: 20px;
-    display: none;
+    display: flex;
+    animation: appear 0.5s 6s forwards;
+    visibility: hidden;
+    border: 10px solid #ffe50a;
+    border-radius: 15px;
+    padding: 20px;
   }
 
+  @keyframes appear {
+    from {
+      visibility: none;
+    }
+    to {
+      visibility: visible;
+    }
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  .profile-picture-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 150px;
+    border: #ffe50a 4px solid;
+    align-self: center;
+  }
+
+  .profile-picture {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .info-container > div {
+    display: flex;
+    gap: 10px;   
+    align-items: center; 
+  }
+
+  .info-container > div > p,
+  .info-container > div > a {
+    font-size: 20px;
+    color: black;
+    font-weight: 700;
+  }
+
+  a.website {
+    text-decoration: none;
+    color: navy;
+  }
 
 .pokeball {
   display: block;
-  width: 192px;
-  height: 192px;
+  width: 200px;
+  height: 200px;
+  position: absolute;
   background: radial-gradient(
       white 16px,
       black 17px 18px,
@@ -54,51 +120,51 @@
     linear-gradient(to bottom, red 0 80px, black 81px 96px, white 97px 100%);
   border-radius: 50%;
   border: 8px solid black;
-  box-shadow: inset -16px -8px 0 0 rgba(0, 0, 0, 0.2);
-  animation: fall 0.5s ease-in-out 1s,
-    shake 1.25s cubic-bezier(0.36, 0.07, 0.19, 0.97) 1.5s 3,
-    catch 0.5s ease-out 5.25s forwards;
+  animation: fall 0.5s,
+    shake 1.25s 1.5s 3,
+    catch 0.5s 5.25s forwards;
   }
-/* Animation */
-@keyframes shake {
-  0% {
-    transform: translateX(0) rotate(0);
+
+  @keyframes shake {
+    0% {
+      transform: translateX(0) rotate(0);
+    }
+    20% {
+      transform: translateX(-10px) rotate(-20deg);
+    }
+    30% {
+      transform: translateX(10px) rotate(20deg);
+    }
+    50% {
+      transform: translateX(-10px) rotate(-10deg);
+    }
+    60% {
+      transform: translateX(10px) rotate(10deg);
+    }
+    100% {
+      transform: translateX(0) rotate(0);
+    }
   }
-  20% {
-    transform: translateX(-10px) rotate(-20deg);
+  @keyframes fall {
+    0% {
+      transform: translateY(-200%);
+    }
+    60% {
+      transform: translateY(0);
+    }
+    80% {
+      transform: translateY(-10%);
+    }
+    100% {
+      transform: translateY(0);
+    }
   }
-  30% {
-    transform: translateX(10px) rotate(20deg);
+
+  @keyframes catch {
+    to {
+      filter: brightness(0.8);
+      display: none;
+    }
   }
-  50% {
-    transform: translateX(-10px) rotate(-10deg);
-  }
-  60% {
-    transform: translateX(10px) rotate(10deg);
-  }
-  100% {
-    transform: translateX(0) rotate(0);
-  }
-}
-@keyframes fall {
-  0% {
-    transform: translateY(-200%);
-  }
-  60% {
-    transform: translateY(0);
-  }
-  80% {
-    transform: translateY(-10%);
-  }
-  100% {
-    transform: translateY(0);
-  }
-}
-@keyframes catch {
-  to {
-    filter: saturate(0.8) brightness(0.8);
-    display: none;
-  }
-}
 
 </style>
