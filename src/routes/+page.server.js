38994b7d165b0526de1d919f86be77d8d704fef4
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-09-17 22:13:31 +0200

    content squad selection

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-import fetchJson from "$lib/fetch-json"
+import fetchJson from "$lib/fetch-json";
 
 export async function load() {
   const url = 'https://fdnd.directus.app/items/squad?filter={"tribe_id":2}';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,43 +7,66 @@
     <h2><a href="/squad/{squad.id}">Squad {squad.name}</a></h2>
   {/each} -->
   <ul>
-    <li id="overview"></li>
+    <li id="overview">
+      <article>
+        <h2>Need help?</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, sint cupiditate fugiat corrupti alias non minus, cum repudiandae fuga placeat perspiciatis deleniti. Quas aperiam, neque fugit cumque iusto similique quia!
+        </p>
+      </article>
+      <img src="src/lib/img/pikachu.png" alt="">
+    </li>
+
+    {#each data.squads as squad}
+    <li>
+      <article>
+        <h2>Squad </h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, sint cupiditate fugiat corrupti alias non minus, cum repudiandae fuga placeat perspiciatis deleniti. Quas aperiam, neque fugit cumque iusto similique quia!
+        </p>
+        <a href="">Squad </a>
+      </article>
+      <img src="src/lib/img/bulbasaur.png" alt="">
+    </li>
+    {/each}
+
+
+
+
     <li id="squad-4">
-      <h2><a href="/squad/4">Squad 2C</a></h2>
+      <article>
+        <h2>Squad 2C</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, sint cupiditate fugiat corrupti alias non minus, cum repudiandae fuga placeat perspiciatis deleniti. Quas aperiam, neque fugit cumque iusto similique quia!
+        </p>
+        <a href="/squad/4">Squad 2C</a>
+      </article>
+      <img src="src/lib/img/bulbasaur.png" alt="">
     </li>
     <li id="squad-5">
-      <h2><a href="/squad/5">Squad 2D</a></h2>
+      <article>
+        <h2>Squad 2D</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, sint cupiditate fugiat corrupti alias non minus, cum repudiandae fuga placeat perspiciatis deleniti. Quas aperiam, neque fugit cumque iusto similique quia!
+        </p>
+        <a href="/squad/5">Squad 2D</a>
+      </article>
+      <img src="src/lib/img/charmander.png" alt="">
     </li>
     <li id="squad-6">
-      <h2><a href="/squad/3">Squad overig</a></h2>
+      <article>
+        <h2>Squad overig</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, sint cupiditate fugiat corrupti alias non minus, cum repudiandae fuga placeat perspiciatis deleniti. Quas aperiam, neque fugit cumque iusto similique quia!
+        </p>
+        <a href="/squad/3">Squad overig</a>
+      </article>
+      <img src="src/lib/img/squirtle.png" alt="">
     </li>
   </ul>
 </main>
 
 <style>
-  /* <Copied from static> */
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500;700&display=swap');
-
-:root {
-
-  /* Colors */
-  --bg-color: #265893;
-  --color-white: #fff;
-  --color-black: #000;
-  --yellow-primary: #FFDA27;
-  --red-primary: #E03343;
-  --blue-primary: #26B7F0;
-  --green-primary: #38B39C;
-
-  /* Fonts */
-  --font-family: "Irish Grover", system-ui;;
-  --font-medium: 500;
-  --font-bold: 700;
-
-  /* shadows */
-  --shadow-regular: 0px 0px 15px 7.5px rgba(0,0,0,0.25);
-}
-  /* </Copied from static> */
   * {
     margin: 0;
     padding: 0;
@@ -59,48 +82,67 @@
   }
 
   main ul {
-    /* flex-grow: 1; */
     display: flex;
     flex-direction: column;
     height: 80%;
     width: 80%;
     background-color: #000;
-    overflow-x: scroll;
+    overflow-y: scroll;
     scroll-snap-type: y mandatory;
     list-style: none;
   }
 
+  main ul li {
+    height: 100%;
+    width: 100%;
+    flex: 0 0 100%;
+    box-sizing: border-box;
+    scroll-snap-align: start;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 2rem 2rem 4rem;
+  }
+
+  main ul li article {
+    position: relative;
+  }
+  main ul li article h2 {
+    font-family: var(--font-family-alt);
+    margin-bottom: 1rem;
+    min-width: 200px;
+  }
+
+   main ul li article h2::after {
+    content: "";
+    background-color: var(--color-black);
+    width: 100px;
+    height: 3px;
+    bottom: -3px;
+    left: 4.5rem;
+    top: 2rem;
+    position: absolute;
+  }
+
+  main ul li article p {
+    max-width: 450px;
+    font-family: var(--font-family);
+  }
+
   main ul li:nth-child(1) {
     background-color: var(--yellow-primary);
-    height: 100%;
-    width: 100%;
-    flex: 0 0 100%;
-    box-sizing: border-box;
-    scroll-snap-align: start;
   }
+
   main ul li:nth-child(2) {
     background-color: var(--green-primary);
-    height: 100%;
-    width: 100%;
-    flex: 0 0 100%;
-    box-sizing: border-box;
-    scroll-snap-align: start;
   }
+
   main ul li:nth-child(3) {
     background-color: var(--red-primary);
-    height: 100%;
-    width: 100%;
-    flex: 0 0 100%;
-    box-sizing: border-box;
-    scroll-snap-align: start;
   }
+
   main ul li:nth-child(4) {
     background-color: var(--blue-primary);
-    height: 100%;
-    width: 100%;
-    flex: 0 0 100%;
-    box-sizing: border-box;
-    scroll-snap-align: start;
   }
 
 </style>
