a18f9fddd37fdbb1fdfed8dbc61d4b185a8c1473
Naddybs <144009698+Naddybs@users.noreply.github.com>
2024-09-20 06:19:09 +0200

    Styling squadpage - versie 2

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,19 +4,118 @@
     export let prefix;
     export let surname;
     export let id;
+    export let color;
   </script>
 
-<main class="card">
+<!-- <main class="card">
     <section class="card-inner">
       <article class="card-front">
-        <img src={avatar} alt="Profile picture of {name}" />
+        <img 
+        src={avatar} 
+        alt="Profile picture of {name}" 
+        onerror="this.onerror=null;this.src='/pikachu.webp';" />
+        <a href='/{id}'>
+        <h2>{name}</h2>
+        </a>
+        <h3>{prefix}{surname}</h3>
+      </article>
+      <div class="card-back">
+        <img src="/pokemon.png" alt="Pokémon background" />
+      </div>
+    </section>
+  </main> -->
+  <main class="card {color}">
+    <section class="card-inner">
+      <article class="card-front">
+        <img 
+        src={avatar} 
+        alt="Profile picture of {name}" 
+        onerror="this.onerror=null;this.src='/pikachu.webp';" />
         <a href='/{id}'>
         <h2>{name}</h2>
         </a>
         <h3>{prefix}{surname}</h3>
       </article>
     </section>
-  </main>
+</main>
   
+  <style>
+    .card {
+    width: 200px;
+    height: 300px;
+    perspective: 1000px;
+    margin: .5em;
+    position: relative;
+    border-radius: 10px;
+    overflow: hidden;
+    /* background: linear-gradient(to bottom right, #46a95c, #92e2a4, #56a267, #0c651f, #59bd6e, #a7eeb6); */
+    box-shadow: 0 4px 20px #000601;
+    border: solid 5px rgba(2, 63, 11, 0.397);
+    transition: transform 0.6s ease, box-shadow 0.6s ease, margin 0.6s ease;
+    padding: .5em;
+    }
+  
+    .card-inner {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      transform-style: preserve-3d;
+      cursor: pointer;
+      transition: transform 0.6s ease;
+    }
+  
+    .card:hover {
+      transform: scale(1.2);
+      z-index: 2;
+      margin: 2em;
+    }
+  
+    .card:hover .card-inner {
+      transform: rotateY(180deg);
+    }
+  
+    .card-front {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      backface-visibility: hidden;
+      border-radius: 0.5em;
+    }
+  
+    .card-front {
+      background-color: rgba(255, 255, 255, 0.167);
+      color: black;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      z-index: 1;
+    }
+  
+    img {
+      width: 180px;
+      height: 150px;
+      object-fit: cover;
+      margin-bottom: 10px;
+      border: solid 5px rgba(224, 235, 10, 0.397);
+      box-shadow: 0 4px 20px #01480a9b;
+    }
+    .card.red {
+    background: linear-gradient(to bottom right, #a90120, #e009aa, #cc1332, #8f080a, #cf034e, #ef6d92);
+  }
+  
+  .card.blue {
+    background: linear-gradient(to bottom right, #1609cc, #2648d0, #722af8, #220680, #484bf3, #5dd3ee);
+  }
+  
+  .card.green {
+    background: linear-gradient(to bottom right, #46a95c, #92e2a4, #56a267, #0c651f, #59bd6e, #a7eeb6);
+  }
+  
+  .card.yellow {
+    background: linear-gradient(to bottom right, #ede901, #f6f876, #dcd926, #938a04, #beb404, #f5e76d);
+  }
 
+  </style>
   
--- src/routes/squad/[squad_id]/+page.svelte
+++ src/routes/squad/[squad_id]/+page.svelte
@@ -4,15 +4,19 @@
 </script>
 
 <main>
+  <span>
   <h1> Squad </h1>
+  <img src="/bulbasaur.webp" alt = "Bulbasaur" />
+  </span>
   <section class ="cards-container">
-    {#each data.persons as person}
+    {#each data.persons as person, index}
         <Card
             name = {person.name}
             avatar = {person.avatar}
             prefix = {person.prefix}
             surname = {person.surname}
             id = {person.id}
+            color = {index % 4 === 0 ? 'red' : index % 4 === 1 ? 'blue' : index % 4 === 2 ? 'green' : 'yellow'}
         />
     {/each}
   </section>
@@ -27,4 +31,33 @@
     gap:.5rem;
     justify-content: center;
   }
+  :global(body){
+    background-color: #78aa90;
+    margin: 0;
+  }
+  h1{
+    color: #ecdc4e;
+  font-weight: 900;
+  text-transform: uppercase;
+  font-size: clamp(3rem, 10vw, 6rem);
+  line-height: 0.75em;
+  text-align: center;
+  text-shadow: 3px 1px 1px #0526429c, 2px 2px 1px #0c07a9, 4px 2px 1px #050b5a, 3px 3px 1px #165bfb, 5px 3px 1px #0b0498, 4px 4px 1px #0080ff, 6px 4px 1px #1f0259, 5px 5px 1px #165bfb, 7px 5px 1px #09017a, 6px 6px 1px #1e044e, 8px 6px 1px #04046b, 7px 7px 1px #165bfb, 9px 7px 1px #0808c5;
+    }
+  span{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    position: relative;
+    height: 10em;
+    background-image: linear-gradient(#a7d2ba, #578474);
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 9vw));
+    & img{
+      width: 30%;
+        height: auto;
+      
+    }
+  }
+
 </style>
